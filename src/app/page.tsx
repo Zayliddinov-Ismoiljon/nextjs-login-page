5a3091645af10117532923ffa23ea843ad19a52d
Zayliddinov-Ismoiljon <zayliddinovismoiljon@gmail.com>
2025-06-11 23:45:21 +0500

    changes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,10 @@
+import Link from "next/link";
+
 export default function HomePage() {
   return (
-    <div className="p-10">
-      <h1 className="text-3xl font-bold">Bosh sahifa (Home Page)</h1>
+    <div className="p-10 flex items-center">
+      <h1 className="text-3xl font-bold">Bosh sahifa</h1>
+      <Link href={'/login'} className="py-2 px-4 bg-green-500 text-white rounded ml-5">Login</Link>
     </div>
   );
 }
